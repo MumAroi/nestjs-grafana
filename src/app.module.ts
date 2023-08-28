8043977824496d6dbe1ea3063c1489f5039a3bb9
Paramas <paramas.w@livinginsider.com>
2023-08-28 17:00:32 +0700

    feat: ✨  dockerfile and loggingInterceptor

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { PrometheusModule } from '@willsoto/nestjs-prometheus';
+import { LoggingInterceptor } from './logging.interceptor';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PrometheusModule.register()],
+  controllers: [],
+  providers: [{
+    provide: APP_INTERCEPTOR,
+    useClass: LoggingInterceptor 
+  }],
 })
 export class AppModule {}
